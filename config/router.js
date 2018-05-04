024607015550b2f86ee9d5943c616ab47ce856d6
Elliot Yandzio <elliotyandzio@gmail.com>
2018-05-04 11:30:13 +0100

    Added the backend controller and route for registration. Can add a new user in insomnia using the post request

--- config/router.js
+++ config/router.js
@@ -1,4 +1,9 @@
 const router = require('express').Router();
-const users = require('../controllers/users');
+// const users = require('../controllers/users');
+const auth = require('../controllers/auth');
 //------------------------------------------------------------------------------
 router.route('/users');
+
+router.post('/register', auth.register);
+
+module.exports= router;
--- index.js
+++ index.js
@@ -5,13 +5,13 @@ const mongoose = require('mongoose');
 //extra--error handler in here if we need.
 
 //Router and DB connect
-// const router = require('./config/router');
+const router = require('./config/router');
 const {port, dbURI} = require('./config/environment');
 mongoose.connect(dbURI);
 
 //App use for parser aswell as connecting api to router
 app.use(bodyParser.json());
-// app.use('/api', router);
+app.use('/api', router);
 //extra--error handler use here if needed.
 
 app.listen(port, () => console.log(`We are within the matrix connected to ${port}`));
