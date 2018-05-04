71707a29507f1c8acaf1688f3de3104d61caf0a3
Elliot Yandzio <elliotyandzio@gmail.com>
2018-05-04 12:17:58 +0100

    Added the front end controller for registration, added the Auth model and edited the App.js

---
+++ src/config/auth.js
@@ -0,0 +1,9 @@
+Auth.$inject = ['$authProvider'];
+
+function Auth($authProvider) {
+  $authProvider.loginUrl = '/api/login';
+  $authProvider.signupUrl = '/api/register';
+
+}
+
+export default Auth;
---
+++ src/controllers/auth/register.js
@@ -0,0 +1,14 @@
+RegisterCtrl.$inject = ['$auth', '$state'];
+
+function RegisterCtrl($auth, $state) {
+  this.data = {};
+
+  function handleRegister() {
+    $auth.signup(this.data)
+      .then(() => $state.go('login'));
+  }
+
+  this.handleRegister = handleRegister;
+}
+
+export default RegisterCtrl;
