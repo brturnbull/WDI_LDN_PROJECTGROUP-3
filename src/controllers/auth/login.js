776571b1ab0a3939958afaeae102c94e9ec3bd68
ExRafiki <shiizzlle00@gmail.com>
2018-05-10 16:52:28 +0100

    reverted changes

--- src/controllers/auth/login.js
+++ src/controllers/auth/login.js
@@ -1,24 +1,26 @@
-LoginCtrl.$inject = ['$auth', '$state'];
+LoginCtrl.$inject = ['$auth', '$state','$rootScope'];
 //-----------------------------------------------------------------------------
-function LoginCtrl($auth, $state,) {
+function LoginCtrl($auth, $state,$rootScope) {
 
   function handleLogin() {
     if(this.form.$invalid)return false;
-    $auth.login(this.data);
-    // .then(() => {
-    //   $rootScope.$broadcast('flashMessage', {
-    //     content: 'You have logged in successfully'
-    //   });
-    $state.go('moodsNew');
+    $auth.login(this.data)
+      .then(() => {
+        $rootScope.$broadcast('flashMessage', {
+          content: 'You have logged in successfully'
+        });
+        $state.go('moodsNew');
+      });
   }
   function authenticate(provider) {
 
-    $auth.authenticate(provider);
-    // .then(() => {
-    //   $rootScope.$broadcast('flashMessage', {
-    //     content: 'You have logged in successfully'
-    //   });
-    $state.go('moodsNew');
+    $auth.authenticate(provider)
+      .then(() => {
+        $rootScope.$broadcast('flashMessage', {
+          content: 'You have logged in successfully'
+        });
+        $state.go('moodsNew');
+      });
   }
   //checking if field has been touched / dirty / submitted
   function isDanger(field){
